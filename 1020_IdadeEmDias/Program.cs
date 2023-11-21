a1e3533d2650b342258ece4e07798b54b8f4e144
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-11-21 08:03:35 -0300

    primeiro commit

---
+++ 1020_IdadeEmDias/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int Dias, idadeDias, idadeAno, idadeMeses;
+        int diaAno = 365;
+        int diaMeses = 30;
+
+        Console.WriteLine("Escreva o número de dias abaixo para descobrir a idade: ");
+        Dias = int.Parse(Console.ReadLine());
+
+        idadeAno = Dias / diaAno;
+        idadeMeses = (Dias - (diaAno * idadeAno)) / diaMeses;
+        idadeDias = (Dias - (diaAno * idadeAno)) - (diaMeses * idadeMeses);
+
+
+        Console.WriteLine(idadeAno + " ano(s)");
+        Console.WriteLine(idadeMeses + " mes(es)");
+        Console.WriteLine(idadeDias + " dia(s)");
+
+    }
+
+}
